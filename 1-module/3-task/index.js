e518fa9c55b4bb93f1ce6dba67b6f21a7b3b9b16
Dmitry Bychkov <d.bychkov.95@mail.ru>
2019-03-12 02:08:22 +0300

    Модуль 1 complete

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,22 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
-
+function compareNumeric(a, b) {
+  	return a - b;
 }
-
+      
+function getMinMax(str) {
+        
+    let arr = str.split(/[\s,]/);
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      	let p = parseFloat(arr[i]);
+      	if (!isNaN(p)) {
+      		newArr.push(p);
+      	}
+    }
+    newArr.sort(compareNumeric);
+    let min = +newArr[0];
+    let max = +newArr[newArr.length - 1];
+    return {min: min, max: max};
+}
