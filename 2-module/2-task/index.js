281ca5d04a46c82ac0cb565128e24974e6173da8
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-04-10 18:30:10 +0200

    Added solution for task: Функция find(obj, value)

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,30 @@
  * @returns {Object}
  */
 function find (obj, value) {
+    let results = [];
+
+    let next = function (obj, path) {
+
+        for (let key in obj) {
+
+            if (obj[key] === value) {
+                results.push(`${path}.${key}`)
+            } else if (typeof obj[key] === 'object' && obj[key] !== null){
+                next(obj[key], `${path}${path ? '.' : ''}${key}`);
+            }
+
+        }
+
+    };
+
+    next(obj, '');
+
+    if (!results.length) {
+        return null;
+    } else if (results.length === 1) {
+        return results[0];
+    } else {
+        return results;
+    }
+
 }
