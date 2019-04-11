c3d26aa60cc8acf474212a6d88fd95d7d7a27349
dmitriybychkov <38225542+dmitriybychkov@users.noreply.github.com>
2019-04-12 00:47:55 +0300

    Add files via upload

---
+++ 3-module/2-task/index.js
@@ -0,0 +1,32 @@
+let calendar = {
+    from: new Date('2018-01-01T15:09:10Z'),
+    to: new Date('2018-02-01T10:09:10Z')
+};
+
+calendar[Symbol.iterator] = function () {
+	  let current = this.from;
+	  let last = this.to;
+
+	  return {
+	    next() {
+	      if (current <= last) {
+	        current.setDate( current.getDate() + 1 );
+	        
+	        let day = current.getDay();
+	        let date = current.getDate() < 10 ? `0${current.getDate()}` : current.getDate();
+	        let val = (day > 0 && day < 6) ? `${date}` : `[${date}]`;
+	        return {
+	          done: false,
+	          value: val
+	        }
+	      } else {
+	        return {
+	          done: true
+	        }
+	      }
+	    }
+
+	  }
+};
+
+
