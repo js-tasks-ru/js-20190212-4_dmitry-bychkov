55a496fa36ea3eac282ff3ec583ef5e62bf27405
dmitriybychkov <38225542+dmitriybychkov@users.noreply.github.com>
2019-04-12 16:51:10 +0300

    mod4

---
+++ 4-module/1-task/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Генерация HTML списка друзей
+ * @param {Friend[]} friends
+ * @return {HTMLUListElement}
+ */
+function makeFriendsList (friends) {
+	let ul = document.createElement('ul');
+	for (let i =0; i < friends.length; i++) {
+		let li = document.createElement('li');
+		li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+		ul.appendChild(li);
+	}
+	return ul;
+}
