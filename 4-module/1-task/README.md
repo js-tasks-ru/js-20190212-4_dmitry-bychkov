1c2e42fe15509e61b3757d5ec5956d5e9cd46f46
Dmitry Bychkov <d.bychkov.95@mail.ru>
2019-04-13 18:09:48 +0300

    reapair

--- 4-module/1-task/README.md
+++ 4-module/1-task/README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ## Функция makeFriendsList(friends) ##
 
 Необходимо реализовать функцию `makeFriendsList`, которая отобразит переданный массив
@@ -21,29 +20,3 @@ let friends = [
     }
 ];
 ```
-
-=======
-## Функция makeFriendsList(friends) ##
-
-Необходимо реализовать функцию `makeFriendsList`, которая отобразит переданный массив
-друзей на странице в виде стандартного html списка (ul, li).
-
-Массив с друзьями имеет следующий формат:
-```javascript
-let friends = [
-    {
-        firstName: 'Artsiom',
-        lastName: 'Mezin'
-    },
-    {
-        firstName: 'Ilia',
-        lastName: 'Kantor'
-    },
-    {
-        firstName: 'Christopher',
-        lastName: 'Michael'
-    }
-];
-```
-
->>>>>>> 32483304f2f33564235ac0ba938fc34992ae7719
--- 4-module/1-task/task.test.js
+++ 4-module/1-task/task.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 describe("4-module-1-task", function() {
 
     let friends = [
@@ -47,54 +46,4 @@ describe("4-module-1-task", function() {
 
 
 
-=======
-describe("4-module-1-task", function() {
-
-    let friends = [
-        {
-            firstName: 'Artsiom',
-            lastName: 'Mezin'
-        },
-        {
-            firstName: 'Ilia',
-            lastName: 'Kantor'
-        },
-        {
-            firstName: 'Christopher',
-            lastName: 'Michael'
-        }
-    ];
-
-    it('функция должна возвращать HTML список', function() {
-        let ul = makeFriendsList(friends);
-
-        expect(ul && ul.constructor).toEqual(HTMLUListElement, 'Результат вызова функции не HTMLUListElement');
-    });
-
-    it('должно быть равно число элментов в списке и число объектов в массиве', function() {
-        let ul = makeFriendsList(friends);
-        let lis = ul && ul.querySelectorAll('li');
-
-        expect(lis.length).toEqual(friends.length, 'Отличается длина списка и массива данных');
-    });
-
-    it (`проверяем содержимое списка`, function () {
-        let ul = makeFriendsList(friends);
-        let lis = ul && ul.querySelectorAll('li');
-
-        lis.forEach(function (li, index) {
-            let actualFirstName = li.innerHTML.includes(friends[index].firstName);
-            let actualLastName = li.innerHTML.includes(friends[index].lastName);
-
-            expect(actualFirstName, `Элемент номер ${index} не содержит firstName`);
-            expect(actualLastName, `Элемент номер ${index} не содержит lastName`);
-        });
-
-    });
-
-
-
-
-
->>>>>>> 32483304f2f33564235ac0ba938fc34992ae7719
 });
